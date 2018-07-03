81c35aafc3504533ae7c9380e11cfe4e0ea38a04
yangya <759106909@qq.com>
2018-07-03 18:03:43 +0800

    单Activity界面架构设计1

---
+++ latte-core/src/main/java/com/yjnull/latte_core/activities/ProxyActivity.java
@@ -0,0 +1,40 @@
+package com.yjnull.latte_core.activities;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.ContentFrameLayout;
+
+import com.yjnull.latte_core.R;
+import com.yjnull.latte_core.delegates.LatteDelegate;
+
+import me.yokeyword.fragmentation.SupportActivity;
+
+/**
+ * Created by Yangya on 2018/7/3
+ */
+public abstract class ProxyActivity extends SupportActivity {
+
+    public abstract LatteDelegate setRootDelegate();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void initContainer(@Nullable Bundle savedInstanceState) {
+        @SuppressLint("RestrictedApi") final ContentFrameLayout container = new ContentFrameLayout(this);
+        container.setId(R.id.delegate_container);
+        setContentView(container);
+        if (savedInstanceState == null) {
+            loadRootFragment(R.id.delegate_container, setRootDelegate());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.gc();
+        System.runFinalization();
+    }
+}
---
+++ latte-core/src/main/java/com/yjnull/latte_core/delegates/LatteDelegate.java
@@ -0,0 +1,7 @@
+package com.yjnull.latte_core.delegates;
+
+/**
+ * Created by Yangya on 2018/7/3
+ */
+public abstract class LatteDelegate extends PermissionCheckerDelegate {
+}
